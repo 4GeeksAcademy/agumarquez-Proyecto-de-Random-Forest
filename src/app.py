d7dcc02362037412a324a921ea229cba0f552fd6
agumarquez10 <agu.marquez10@gmail.com>
2025-12-01 19:39:13 +0000

    prueba 6

--- src/app.py
+++ src/app.py
@@ -16,6 +16,13 @@ if not os.path.isfile(MODEL_PATH):
 
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
+DSBASE_DIR = os.path.dirname(__file__)   # .../src
+DSDATA_PATH = os.path.join(BASE_DIR, "..", "data", "dataset.pkl")
+
+if not os.path.isfile(DSDATA_PATH):
+    raise FileNotFoundError(f"Dataset no encontrado en: {DSDATA_PATH}")
+
+df = pd.read_pickle(DSDATA_PATH)
 
 # Diccionario de clases
 class_dict = {
